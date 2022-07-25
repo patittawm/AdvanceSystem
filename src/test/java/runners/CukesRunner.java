9b68c18640bcc1bcf5d5366f38144b5b2beb429a
patittawm <spicielover@gmail.com>
2022-07-24 18:52:05 -0700

    set

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = {"step_definitions"},
         stepNotifications = true,
-        dryRun = true,
+        dryRun = false,
         tags = "@ADVSYS-12"
 )
 public class CukesRunner {
